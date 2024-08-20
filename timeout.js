b124495ca252bbbd8ddfac49b1a4a262f9606e88
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-20 13:56:09 +0600

    timeout

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomthing() {
+    console.log('I am coding Javascript');
+}
+
+console.log('First: ami sobar age');
+console.log('Socond: I am the second person');
+setTimeout(function () {
+    console.log('I am using VS Code')
+}, 5000);
+setTimeout(() => {
+    console.log('Exploring MDN articles');
+}, 4000)
+console.log('Third: ami 3 number baccha');
+console.log('Fourth: ami ek hali purno korsi');
